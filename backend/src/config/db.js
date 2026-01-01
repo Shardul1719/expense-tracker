987ccccacf12779d60bc7f4fb69120beee5c9948
Shardul1719 <shardulj26@gmail.com>
2026-01-01 12:28:52 +0530

    Fix MySQL env variables for Railway

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,34 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import mysql from "mysql2/promise";
+
+// console.log("DB USER 👉", process.env.DB_USER);
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
+// export default pool;
+
 import dotenv from "dotenv";
 dotenv.config();
-
 import mysql from "mysql2/promise";
 
-console.log("DB USER 👉", process.env.DB_USER);
-
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
 });
 
 export default pool;
+
